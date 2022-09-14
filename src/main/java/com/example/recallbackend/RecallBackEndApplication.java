f9c1b763ee79bbc1d29c84a2be6244ba18f285a3
tzihklearn <1922941549@qq.com>
2022-09-14 13:55:52 +0800

    feat：初始化项目

---
+++ src/main/java/com/example/recallbackend/RecallBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.recallbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecallBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecallBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recallbackend/RecallBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recallbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecallBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
